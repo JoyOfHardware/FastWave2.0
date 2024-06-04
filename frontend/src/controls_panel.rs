e3e3820cc53b1fcbf1494e9330c5a29f992a9596
Martin Kavík <martin@kavik.cz>
2024-06-04 20:46:10 +0200

    file picker - file input

--- frontend/src/controls_panel.rs
+++ frontend/src/controls_panel.rs
@@ -5,7 +5,7 @@ use std::mem;
 use std::ops::Not;
 use std::rc::Rc;
 use wellen::GetItem;
-use zoon::*;
+use zoon::{println, *};
 
 const SCOPE_VAR_ROW_MAX_WIDTH: u32 = 480;
 const MILLER_COLUMN_MAX_HEIGHT: u32 = 500;
@@ -124,6 +124,7 @@ impl ControlsPanel {
             ))
     }
 
+    #[cfg(FASTWAVE_PLATFORM = "TAURI")]
     fn load_button(&self) -> impl Element {
         let (hovered, hovered_signal) = Mutable::new_and_signal(false);
         let hierarchy_and_time_table = self.hierarchy_and_time_table.clone();
@@ -152,7 +153,7 @@ impl ControlsPanel {
                 let hierarchy_and_time_table = hierarchy_and_time_table.clone();
                 let loaded_filename = loaded_filename.clone();
                 Task::start(async move {
-                    if let Some(filename) = platform::pick_and_load_waveform().await {
+                    if let Some(filename) = platform::pick_and_load_waveform(None).await {
                         loaded_filename.set_neq(Some(filename));
                         let (hierarchy, time_table) =
                             join!(platform::get_hierarchy(), platform::get_time_table());
@@ -161,6 +162,77 @@ impl ControlsPanel {
                     }
                 })
             })
+    }
+
+    #[cfg(FASTWAVE_PLATFORM = "BROWSER")]
+    fn load_button(&self) -> impl Element {
+        let (hovered, hovered_signal) = Mutable::new_and_signal(false);
+        let hierarchy_and_time_table = self.hierarchy_and_time_table.clone();
+        let loaded_filename = self.loaded_filename.clone();
+        let file_input_id = "file_input";
+        Row::new()
+            .item(
+                Label::new()
+                    .s(Padding::new().x(20).y(10))
+                    .s(Background::new().color_signal(
+                        hovered_signal.map_bool(|| color!("MediumSlateBlue"), || color!("SlateBlue")),
+                    ))
+                    .s(Align::new().left())
+                    .s(RoundedCorners::all(15))
+                    .s(Cursor::new(CursorIcon::Pointer))
+                    .label(El::new().s(Font::new().no_wrap()).child_signal(
+                        loaded_filename.signal_cloned().map_option(
+                            |filename| format!("Unload {filename}"),
+                            || format!("Load file.."),
+                        ),
+                    ))
+                    .on_hovered_change(move |is_hovered| hovered.set_neq(is_hovered))
+                    .for_input(file_input_id)
+                    .on_click_event_with_options(EventOptions::new().preventable(), clone!((hierarchy_and_time_table) move |event| {
+                        let mut hierarchy_and_time_table_lock = hierarchy_and_time_table.lock_mut();
+                        if hierarchy_and_time_table_lock.is_some() {
+                            *hierarchy_and_time_table_lock = None;
+                            if let RawMouseEvent::Click(raw_event) = event.raw_event {
+                                // @TODO Move to MoonZoon as a new API
+                                raw_event.prevent_default();
+                            }
+                            return;
+                        }
+                    }))
+            )
+            .item(
+                // @TODO https://github.com/MoonZoon/MoonZoon/issues/39
+                // + https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications#using_hidden_file_input_elements_using_the_click_method
+                TextInput::new()
+                    .id(file_input_id)
+                    .update_raw_el(|raw_el| {
+                        let dom_element = raw_el.dom_element();
+                        raw_el
+                            .style("display", "none")
+                            .attr("type", "file")
+                            .event_handler(move |_: events::Input| {
+                                let Some(file_list) = dom_element.files().map(gloo_file::FileList::from) else {
+                                    println!("file list is `None`");
+                                    return;
+                                };
+                                let Some(file) = file_list.first().cloned() else {
+                                    println!("file list is empty");
+                                    return;
+                                };
+                                let hierarchy_and_time_table = hierarchy_and_time_table.clone();
+                                let loaded_filename = loaded_filename.clone();
+                                Task::start(async move {
+                                    if let Some(filename) = platform::pick_and_load_waveform(Some(file)).await {
+                                        loaded_filename.set_neq(Some(filename));
+                                        let (hierarchy, time_table) =
+                                            join!(platform::get_hierarchy(), platform::get_time_table());
+                                        hierarchy_and_time_table
+                                            .set(Some((Rc::new(hierarchy), Rc::new(time_table))))
+                                    }
+                                })
+                        })
+                    })
+            )
     }
 
     fn layout_switcher(&self) -> impl Element {
