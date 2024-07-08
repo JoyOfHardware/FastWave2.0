6cfcdc6d0ba7415bdb52297ea7be1ec7ef56be88
Martin Kavík <martin@kavik.cz>
2024-07-08 14:38:30 +0200

    fix Python decoder

--- test_files/components/python_decoder/src/app.py
+++ test_files/components/python_decoder/src/app.py
@@ -5,7 +5,8 @@ name = "Python Test Decoder"
 
 class Decoder(exports.Decoder):
     def init(self) -> None:
-        # @TODO it panics for some reasons 
+        # @TODO it panics with error `7: 0xae8683 - libcomponentize_py_runtime.so!componentize-py#Dispatch`
+        # - see https://github.com/bytecodealliance/componentize-py/blob/e20d9e6706ff1421cd8001449acb51eb9c87d0c6/runtime/src/lib.rs#L404
         # host.log(f"{name} initialized")
         return None
 
@@ -13,4 +14,4 @@ class Decoder(exports.Decoder):
         return name
 
     def format_signal_value(self, value: str) -> str:
-        return str + "!"
+        return value + "!"
