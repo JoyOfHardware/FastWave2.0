a99915c8c3ef5aa71a62bf495693f599e088a7b3
Martin Kavík <martin@kavik.cz>
2024-10-08 14:40:01 +0200

    excalidraw_canvas.rs

--- frontend/src/diagram_panel.rs
+++ frontend/src/diagram_panel.rs
@@ -1,13 +1,20 @@
 use zoon::*;
 
+mod excalidraw_canvas;
+use excalidraw_canvas::ExcalidrawCanvas;
+pub use excalidraw_canvas::ExcalidrawController;
+
 #[derive(Clone)]
 pub struct DiagramPanel {
+    canvas_controller: Mutable<Mutable<Option<SendWrapper<ExcalidrawController>>>>
 }
 
 impl DiagramPanel {
     pub fn new(
+        canvas_controller: Mutable<Mutable<Option<SendWrapper<ExcalidrawController>>>>,
     ) -> impl Element {
         Self {
+            canvas_controller
         }
         .root()
     }
@@ -20,5 +27,18 @@ impl DiagramPanel {
             .s(Height::fill())
             .s(Gap::new().y(20))
             .item("Diagram panel")
+            .item(self.canvas())
+    }
+
+    fn canvas(&self) -> impl Element {
+        let canvas_controller = self.canvas_controller.clone();
+        ExcalidrawCanvas::new()
+            .s(Align::new().top())
+            .s(Width::fill())
+            .s(Height::fill())
+            .task_with_controller(move |controller| {
+                canvas_controller.set(controller.clone());
+                println!("hello from task_with_controller")
+            })
     }
 }
