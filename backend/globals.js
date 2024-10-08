f05c489d62dea0d9a0875ac21fa4ee1b966fc60d
Martin Kavík <martin@kavik.cz>
2024-10-08 22:34:32 +0200

    local excalidraw assets

--- backend/globals.js
+++ backend/globals.js
@@ -1,5 +1,7 @@
 // -- Excalidraw settings --
-// @TODO replace with "true" once Preact is integrated to ExcalidrawCanvas
+// @TODO replace with "true" once Preact is integrated into ExcalidrawCanvas
 var process = { env: { IS_PREACT: "false" } };
+// @TODO probably remove or update once Preact is integrated into ExcalidrawCanvas
 window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = { isDisabled: true };
+window.EXCALIDRAW_ASSET_PATH = "/_api/public/excalidraw/";
 // -- / --
